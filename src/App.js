cc8afc6ba86c86c48ed4209ebf524016fcfbd7ee
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-02-01 22:57:13 +0200

    Add service GetAllQuotes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { RandomQuote } from './components/RandomQuote';
 import { SingleQuote } from './components/SingleQuote';
+import { AllQuotesList } from './components/AllQuotesList';
 
 function App() {
     return (
 	    <div className="App">
 	    <BrowserRouter>
 	    <Switch>
+	    <Route path='/quotes/all' exact component={AllQuotesList} />
 	    <Route path='/quotes/:id' exact component={SingleQuote} />
 	    </Switch>
 	    </BrowserRouter>
